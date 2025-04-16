fe1cf8f08f6bd885b6e377007cd9dcdab4691592
Dinesh Bobburu <bobburudinesh123@gmail.com>
2025-04-16 11:58:58 -0500

    Initial Application Setup Files.

---
+++ Core/Inc/main_app.h
@@ -0,0 +1,14 @@
+/*
+ * main_app.h
+ *
+ *  Created on: Apr 16, 2025
+ *      Author: dineshbobburu
+ */
+
+#ifndef INC_MAIN_APP_H_
+#define INC_MAIN_APP_H_
+
+#include "stm32f4xx_hal.h"
+
+
+#endif /* INC_MAIN_APP_H_ */
---
+++ Core/Src/main_app.c
@@ -0,0 +1,17 @@
+/*
+ * main_app.c
+ *
+ *  Created on: Apr 16, 2025
+ *      Author: dineshbobburu
+ */
+
+
+#include <main_app.h>
+
+int main(void) {
+	HAL_Init();
+	while(1) {
+
+	}
+	return 0;
+}
